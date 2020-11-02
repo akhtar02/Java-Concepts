09141ee26c139718430fa92509a5d53db9b7f6a1
akhtar hussain <akhtarhussain0788@gmail.com>
2020-11-03 00:01:28 +0500

    Concept

---
+++ if_else_statement.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+   int num=70;
+      if( num < 100 ){
+    System.out.println("number is less than 100");
+  }
+  else{
+    System.out.println("number is greater than 100");
+  }}}
